b1a09e398f7a95857567d27dbb2d10b30a139cae
Gustavojqm <155920691+Gustavojqm@users.noreply.github.com>
2024-01-07 18:11:19 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-alert('Sabias que ibas a decir que si :D')
+alert('Sabia que no podrias dejar de amarme jaja')
 
 });
 
